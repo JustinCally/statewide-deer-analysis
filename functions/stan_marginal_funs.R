9a14d052f0588d78a93f230c755273f82e10de76
Justin Cally <36715313+JustinCally@users.noreply.github.com>
2023-08-01 17:50:44 +1000

    add more plots

--- functions/stan_marginal_funs.R
+++ functions/stan_marginal_funs.R
@@ -1,4 +1,4 @@
-marginal_effects_cmd <- function(draws, param, param_number, model_data, model_column, pwr = 1, log = FALSE, transition = FALSE) {
+marginal_effects_cmd <- function(draws, param, param_number, model_data, model_column, pwr = 1, log = FALSE, transition = FALSE, abundance = FALSE) {
   require(data.table)
   intercept = paste0(param, "[1]")
   if(class(model_data[[model_column]]) == "factor") {
@@ -32,11 +32,11 @@ marginal_effects_cmd <- function(draws, param, param_number, model_data, model_c
 
     if(transition) {
       effect[,c] <- plogis(alpha[[1]] + model_draws[[1]] * mr + model_draws[[2]] * i) - plogis(alpha[[1]]) # scaled so no need to add additional mean vals
+    } else if(abundance) {
+      effect[,c] <- model_draws[[2]] * i
     } else {
       effect[,c] <- plogis(model_draws[[1]] * mr + model_draws[[2]] * i) # scaled so no need to add additional mean vals
     }
-
-
     c = c+1
   }
 
@@ -83,6 +83,38 @@ marginal_effects_plot_cmd <- function(data, col, factor = FALSE, trans = "identi
       ggplot2::scale_x_continuous(trans = trans) +
       # ylim(c(0,47)) +
       ggplot2::theme_classic() +
+      ggplot2::theme(axis.title.y = ggtext::element_markdown())
+  }
+  return(plot)
+}
+
+marginal_effects_plot_cmd_all <- function(data, col, factor = FALSE, trans = "identity", ylab = "Occupancy Probability") {
+  data_sum <- data %>%
+    dplyr::group_by(variable, species, param) %>%
+    dplyr::summarise(median = median(value),
+                     q5 = quantile(value, 0.025),
+                     q25 = quantile(value, 0.25),
+                     q75 = quantile(value, 0.75),
+                     q95 = quantile(value, 0.975))
+
+  if(factor) {
+    plot <- data %>%
+      ggplot2::ggplot(aes(x = variable, y = value)) +
+      ggplot2::geom_violin(draw_quantiles = c(0.05, 0.5, 0.95), fill = col, alpha = 0.5) +
+      ggplot2::ylab(ylab) +
+      # ylim(c(0,47)) +
+      ggplot2::theme_classic() +
+      ggplot2::theme(axis.title.y = ggtext::element_markdown())
+  } else{
+
+    plot <- data_sum %>%
+      ggplot2::ggplot(aes(x = variable, y = median)) +
+      ggplot2::geom_ribbon(aes(ymin = q5, ymax = q95), alpha = 0.25, fill = "grey20") +
+      ggplot2::geom_ribbon(aes(ymin = q25, ymax = q75), alpha = 0.25, fill = "grey20") +
+      ggplot2::geom_line(linewidth = 1.5, colour = "grey20") +
+      ggplot2::ylab(ylab) +
+      ggplot2::scale_x_continuous(trans = trans) +
+      # ylim(c(0,47)) +
       ggplot2::theme(axis.title.y = ggtext::element_markdown())
   }
   return(plot)
