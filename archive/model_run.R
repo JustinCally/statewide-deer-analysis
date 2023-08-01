7e700ff072bafc9c98894ddc2c678665683008b6
Justin Cally <36715313+JustinCally@users.noreply.github.com>
2023-08-01 11:40:21 +1000

    model run script

--- archive/model_run.R
+++ archive/model_run.R
@@ -1,11 +1,29 @@
+# read in data
+model_data_file <- "data/model_data.rds"
+model_data <- readRDS(model_data_file)
+
+# list deer species to model
+deer_species_all <- c("Cervus unicolor", "Dama dama", "Cervus elaphus", "Axis porcinus")
+
+# mcmc params
+ni <- 750 # sampling iterations
+nw <- 750 # warmup iterations
+nc <- 8 # number of chains
+
+# read in models
+model_poisson <- cmdstan_model(here::here("stan", "count_det_nondet_poisson_hs.stan"))
+model_poisson_co <- cmdstan_model(here::here("stan", "count_only_poisson_hs.stan"))
+
+# specify which models to run (hog deer cant use transect info)
+integrated_model <- c(TRUE, TRUE, TRUE, FALSE)
 distributions <- c("poisson")
 model_fits <- list()
 
 for(j in 1:length(distributions)) {
 
-  for(i in 4:length(deer_species_all)) {
+  for(i in 1:length(deer_species_all)) {
 
-    if(evaluate_transects[i]) {
+    if(integrated_model[i]) {
       model_to_fit <- get(paste0("model_", distributions[j]))
     } else {
       model_to_fit <- get(paste0("model_", distributions[j], "_co"))
@@ -17,8 +35,8 @@ for(j in 1:length(distributions)) {
                                            init = 0.1,
                                            max_treedepth = 10,
                                            refresh = 20,
-                                           step_size = 0.1,
-                                           adapt_delta = 0.99,
+                                           step_size = 0.001,
+                                           adapt_delta = 0.999,
                                            show_messages = TRUE,
                                            save_warmup = FALSE,
                                            iter_sampling = ni,
